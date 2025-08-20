c257851764e9116f3c7a2135995043756becbdb7
Page <pagepoako@gmail.com>
2025-08-20 18:25:46 +0800

    feat: implement operators and expressions
    - Add mathematical operators (+, -, *, /, **)
    - Implement assignment operators (+=, -=, *=, /=, ++, --)
    - Create comparison operators for boolean logic
    - Understand operator precedence and parentheses
    - Complete BMI Calculator coding challenge with real-world

--- 01-fundamentals-part-1/starter/starter/script.js
+++ 01-fundamentals-part-1/starter/starter/script.js
@@ -73,3 +73,93 @@
 // //change to a boolean
 // dynamicVariable = true;
 // console.log(dynamicVariable, typeof dynamicVariable);
+
+console.log('=== MATH OPERATORS ===');
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+console.log('Math operations');
+console.log("Addition:", 10 + 5);
+console.log("Subtraction:", 20 - 8);
+console.log("Multiplication:", 4 * 7);
+console.log("Division:", 15 / 3);
+console.log("Exponentiation:", 2 ** 3);
+
+//Math with Strings
+const firstName = "Page";
+const lastName = "Aurellano";
+console.log("Concatenations:", firstName + " " + lastName);
+
+console.log("Hello, " + "World" + "!");
+
+//Assigment Operators
+console.log("=== ASSIGNMENT OPERATORS ===");
+
+let x = 10 + 5;
+console.log("x starts as:", x);
+
+x += 10;
+console.log("x starts as:", x);
+
+x *= 4;
+console.log("x starts as:", x);
+
+x /= 2;
+console.log("x starts as:", x);
+
+x++;
+console.log("x starts as:", x);
+
+x--;
+console.log("x starts as:", x);
+
+// comparison operators
+console.log("=== COMPARISON OPERATIONS ===");
+
+console.log("Age Comparison: ");
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+console.log(ageJonas < 30);
+
+console.log("Number Comparison");
+console.log(25 > 20);
+console.log(15 > 10);
+console.log(18 >= 18);
+console.log(16 <= 15);
+
+const isFullAge = ageSarah >= 18;
+
+console.log("Sarah is an adult:",  isFullAge);
+
+console.log("Complex comparison");
+console.log(now - 1991 > now - 2018);
+
+let z, y;
+
+z = y = 25 - 10 - 5;
+console.log(z, y);
+
+const averageAge = (ageJonas + ageSarah)/2;
+console.log(ageJonas, ageSarah, averageAge);
+
+//Coding Challenge #1 - BMI Calculator
+console.log("=== Coding Challenge #1 ===");
+const massMark = 78;
+const heightMark = 1.69;
+const markBMI = massMark / heightMark ** 2;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+const johnBMI = massJohn / heightJohn ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log("Marks BMI:", markBMI);
+console.log("John BMI:", johnBMI);
+console.log("Is Mark's BMI Higher than John?",markHigherBMI);
+
+
